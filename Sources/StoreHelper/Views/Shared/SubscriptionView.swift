8c28be07376bc69f5ed60eff626afc858ff53706
Naren <naren@alpastechnology.com>
2023-07-26 23:21:57 +0545

    Testing Subscription Info

--- Sources/StoreHelper/Views/Shared/SubscriptionView.swift
+++ Sources/StoreHelper/Views/Shared/SubscriptionView.swift
@@ -61,11 +61,13 @@ public struct SubscriptionView: View {
 //                .onTapGesture { productInfoCompletion(productId) }
             
             PurchaseButton(purchaseState: $purchaseState, productId: productId, price: price, signPromotionalOffer: signPromotionalOffer)
-            
+            Text("Purchase State " + purchaseState.shortDescription())
+            Text("Subscription Info " + (subscriptionInfo?.subscriptionStatus.debugDescription ?? "NOPE"))
             if purchaseState == .purchased, subscriptionInfo != nil {
                 SubscriptionInfoView(subscriptionInfo: subscriptionInfo!)
             } else {
-                ProductInfoView(productId: productId, displayName: displayName, productInfoCompletion: productInfoCompletion)
+                EmptyView()
+//                ProductInfoView(productId: productId, displayName: displayName, productInfoCompletion: productInfoCompletion)
             }
             
             Divider()
